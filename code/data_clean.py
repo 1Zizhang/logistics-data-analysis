0b0d37db56294282caa23adee240d8821e690f5a
zhanghaoxuan <mes_Zhanghaoxuan@outlook.com>
2026-08-16 04:12:56 +0800

    对数据进行清洗，后续进行预处理

---
+++ code/data_clean.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+
+def load_and_clean_data(file_path: str):
+    # 数据清洗（重复，缺失，格式调整）
+    data = pd.read_csv('../data/data_wuliu.csv', encoding='gbk')
+    # 通过info函数可以看出有的列数据有缺失(订单号,货品交货状况,数量)，订单行删除，销售额列格式不对，需要调整转换
+    # 删除重复记录
+    data.drop_duplicates(keep='first', inplace=True)  # 遇到重复保留第一行，并修改数据
+    # 删除缺失值(有NA就删除整行)
+    data.dropna(axis=0, how='any', inplace=True)
+    # 删除订单行
+    data.drop(columns=['订单行'], inplace=True)
+    # 更新索引
+    data.reset_index(drop=True, inplace=True)
+
+    # 调整金额格式
+    def data_deal(number):
+        if number.find('万元') != -1:
+            number_new = float(number[:number.find('万元')].replace(',', '')) * 10000
+        elif number.find('元') != -1:
+            number_new = float(number.replace('元', '').replace(',', ''))
+        else:
+            number_new = number
+        return number_new
+
+    data['销售金额'] = data['销售金额'].map(data_deal)
+    return data
